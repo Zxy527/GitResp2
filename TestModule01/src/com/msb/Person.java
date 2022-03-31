3c99c7eed946779b415c234323cad7b1756adce0
zxy <1185213538@qq.com>
2022-03-31 17:00:16 +0800

    在person类中增加了age属性

---
+++ TestModule01/src/com/msb/Person.java
@@ -0,0 +1,11 @@
+package com.msb;
+
+/**
+ * @Author: zxy
+ * @Date: 2022/3/31 - 03 - 31 - 16:59
+ * @Description: com.msb
+ * @version: 1.0
+ */
+public class Person {
+    private int age;//age
+}
